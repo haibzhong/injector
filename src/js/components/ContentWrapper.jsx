69ab9ef276874f76abfd709e05a0e198f0dc5729
jason.yin@aliyun.com <jason.yin@aliyun.com>
2016-02-03 17:52:21 +0800

    add semantic-ui support

--- src/js/components/ContentWrapper.jsx
+++ src/js/components/ContentWrapper.jsx
@@ -4,7 +4,9 @@ export default class ContentWrapper extends React.Component {
     render() {
         return (
             <div id="content">
-                
+                <button className="ui button">
+                    Add
+                </button>
             </div>
         )
     }
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,6 @@
+import '../css/semantic.min.css';
 import '../css/popup.scss';
+import 'semantic-ui';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ContentWrapper from './components/ContentWrapper.jsx';
